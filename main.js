37b92962f359866d76a06b7fadc4c65c44e9d883
lsj0317 <90132197+lsj0317@users.noreply.github.com>
2022-04-20 16:42:24 +0900

    typing-game

---
+++ main.js
@@ -0,0 +1,115 @@
+
+
+// 사용 변수
+const SETTING_TIME = 10; // 게임 타임 10초
+let words = [];
+let time;
+let isPlaying = false;
+let score = 0;
+
+
+const url = "https://random-word-api.herokuapp.com/word?number=100"; // 랜덤한 단어 가져오기
+const timeDisplay = document.querySelector('.time')
+const button = document.querySelector('.button')
+const wordDisplay = document.querySelector('.word-display')
+const wordInput = document.querySelector('.word-input')
+const scoreDisplay = document.querySelector('.score')
+
+
+init();
+function init() {
+    getWords();
+    wordInput.addEventListener('input', checkMatch)
+}
+
+
+function checkStatus() {
+    if (!isPlaying && time === 0) {
+        isPlaying = false;
+        buttonChange('start', '게임시작');
+        clearInterval(checkInterval)
+    }
+}
+
+function checkMatch() {
+    if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
+        wordInput.value = "";
+        if (!isPlaying) {
+            runNotification('error')
+            return
+        }
+        score++;
+        scoreDisplay.innerText = score;
+        time = SETTING_TIME;
+        const randomIndex = Math.floor(Math.random() * words.length)
+        wordDisplay.innerText = words[randomIndex];
+        runNotification('success')
+    }
+}
+
+
+
+
+function run() { // 게임 실행
+    if (words.length < 1) {
+        return;
+    }
+    wordInput.value = "";
+    wordInput.focus() // 커서 포커스가 되도록
+    score = 0;
+    scoreDisplay.innerText = 0; // 점수 텍스트를 0점으로 초기화시킬수있게
+    time = SETTING_TIME;
+    isPlaying = true;
+    timeInterval = setInterval(countDown, 1000)
+    checkInterval = setInterval(checkStatus, 50)
+    buttonChange('loading', '게임중')
+}
+
+function countDown() {
+    time > 0 ? time-- : isPlaying = false;
+    timeDisplay.innerText = time;
+    if (!isPlaying) {
+        clearInterval(timeInterval)
+    }
+    console.log('count')
+}
+
+function getWords() { // 단어 불러오기
+    axios.get(url).then((res) => {
+
+        res.data.forEach((word) => {
+            if (word.length < 7) {
+                words.push(word);
+            }
+            buttonChange('start', '게임시작')
+        })
+    }).catch((err) => {
+        console.log(err);
+    })
+}
+
+function buttonChange(type, text) {
+    button.innerText = text;
+    type === 'loading' ? button.classList.add('loading') : button.classList.remove('loading')
+}
+
+
+
+function runNotification(type) {
+    // toastify options
+    const option = {
+        text: `${wordDisplay.innerText}!!`,
+        duration: 3000,
+        newWindow: true,
+        gravity: "top", // `top` or `bottom`
+        position: 'left', // `left`, `center` or `right`
+        backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)"
+    }
+    if (type === 'error') {
+        option.text = '우선 게임시작 버튼을 눌러주세요'
+        option.position = 'right'
+        option.backgroundColor = 'red'
+    }
+
+    Toastify(option).showToast();
+}
